725f6580ef3675388e2846dec60293d901999a4e
Glockee <douglaskota@gmail.com>
2023-08-20 10:37:55 -0300

    Atualizando dia 02 main.js

--- Dia_02/02_main.js
+++ Dia_02/02_main.js
@@ -1,20 +1,15 @@
-alert ("Bem Vindo ao Dia 02 do Desafio 7 day of code da Alura!");
-
 const nome = prompt("Qual o seu nome?");
-nome;
 
 const idade = prompt("Quantos anos você tem?");
-idade;
 
 const linguagemProgramacao = prompt("Qual linguagem de programação você está estudando?");
-linguagemProgramacao;
 
 const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagemProgramacao}!`;
+
 alert(msg);
 
 //Exercicio Opcional
 const estudarLinguagem = prompt("Você gosta de estudar " + linguagemProgramacao + "? Responda com o número 1 para SIM ou 2 para NÃO.");
-estudarLinguagem;
 
 if (estudarLinguagem == 1) {
     alert("Muito bom! Continue estudando e você terá muito sucesso.");
