0772b815c7e48327acc1f4101f997488c511749e
Glockee <douglaskota@gmail.com>
2023-08-21 14:01:40 -0300

    alteracao dia 03

--- Dia_03/03_main.js
+++ Dia_03/03_main.js
@@ -1,12 +1,40 @@
 alert("Bem vindo ao jogo de escolhas de destinos! Espero que goste!");
 
 const area = prompt("Qual área você gostaria de seguir Front-End ou Back-End?");
+let linguagem = "";
 
 if (area === 'Front-End') {
-    const frontEnd = prompt ("Em Front-End, você gostaria de seguir em React ou Vue?");
+    linguagem = prompt ("Em Front-End, você gostaria de seguir em React ou Vue?");
 } else if(area === 'Back-End'){
-    const backEnd = prompt ("Em Back-End, você gostaria de seguir em C# ou Java?");
+    linguagem = prompt ("Em Back-End, você gostaria de seguir em C# ou Java?");
 }else {
     alert("Você digitou uma resposta inválida, por favor digita corretamente a resposta de acordo com a pergunta!");
 }
 
+const desenvolvimento = prompt ("Você gostaria de seguir se especializando na area ou se desenvolver para se tornar um FullStack, digite Especializar ou FullStack !");
+
+if (desenvolvimento === 'Especializar') {
+    alert(`Excelente continue se especializando em ${linguagem} para dominar a área ${area}`);
+} else if(desenvolvimento === 'FullStack'){
+    alert(`Continue estudando outras linguagens além da ${linguagem} para se tornar um FullStack`);
+}else {
+    alert("Você digitou uma resposta inválida, por favor digita corretamente a resposta de acordo com a pergunta!");
+}
+
+let listaTecnologia = [];
+
+let encerrarJogo ='Continuar';
+
+while (encerrarJogo !== 'Sair') {
+    
+     let resposta = prompt ("Quais são as tecnologias nas quais a você gostaria de se especializar ou de conhecer? Caso não tenha mais nenhuma digite Sair !");
+     
+    if (resposta !== 'Sair') {
+        listaTecnologia.push(resposta);
+        alert(`${resposta} é realmente uma tecnologia muito legal!`);
+    }else{
+        encerrarJogo = resposta;
+    }
+
+   console.log(listaTecnologia);
+}   
