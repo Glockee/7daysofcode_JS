0e167cf4e1eb3dcdddc02f9012b9f02763479dcd
Glockee <douglaskota@gmail.com>
2023-08-22 13:58:20 -0300

    Arrumando a resposta dia 04

--- Dia_04/04_main.js
+++ Dia_04/04_main.js
@@ -2,15 +2,18 @@ alert("Este é um jogo de adivinhação !")
 
 const numero = Math.floor(Math.random() * (10 - 0 + 1) + 0);
 console.log(numero);
+let pergunta = "";
+let acertou = false;
 
 for(let contador = 0; contador < 3; contador ++) {
       
-    const pergunta = prompt("Tente adivinhar o número em que estou pensando, escolha um número de 0 a 10!");
+    pergunta = prompt("Tente adivinhar o número em que estou pensando, escolha um número de 0 a 10!");
 
     if(pergunta == numero) {
     
         alert(`Você acertou o número em que eu pensei é ${numero}!`);
-        contador = 3
+        let acertou = true;
+        contador = 3;
         
     }else {
         
@@ -20,6 +23,9 @@ for(let contador = 0; contador < 3; contador ++) {
 
 }
 
+if(acertou == false) {
+    alert(`Infelizmente você não acertou o número em que pensei era ${numero}!`);
+}
 
 
 
